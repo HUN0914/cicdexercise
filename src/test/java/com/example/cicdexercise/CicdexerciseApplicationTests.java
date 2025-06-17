a7716f09586571bdf738e42523c88ebafeab7c16
Hun0914 <bkh010914@naver.com>
2025-06-17 15:05:31 +0900

    init : 초기 설정

---
+++ src/test/java/com/example/cicdexercise/CicdexerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdexerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
